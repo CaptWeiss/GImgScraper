0fd3d42a682f7f72cc134bc9c0085b59ba63a9a1
Capt Weiss <captweiss@foxmail.com>
2021-09-24 10:30:59 +0100

    change saveImage function to async
    Signed-off-by: Capt Weiss <captweiss@foxmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# Google Image Scraper
+
+## Installation
+
+- Clone the repo and run `yarn` to install required packages.
+- It's important you create two directories (folder) if they don't already exist.
+ > `images` & `imagesLink` folder.
+
+## Usage
+
+To scrape any image simply run `yarn scrape "horse rider"` where "horse rider" can me any image keyword you desire.
+
+The whole returned image links will be saved as a json file under your `imagesLink` folder and images will be saved under the `images` folder
--- index.js
+++ index.js
@@ -6,18 +6,28 @@ if (!globalThis.fetch) {
     globalThis.fetch = fetch;
 }
 
-const google = new Scraper({
+const google = new Scraper({ // initailize
     puppeteer: {
-        timeout: 120000
+        // this is added to extend timeout error incase you are experiencing one. if error persit,
+        // the temporary fix which only applies locally is to dig into the npm package and locate
+        // the file path: node_modules/images-scraper/src/google/scraper.js 
+        // then goto "_scrapePage" function and add `page.setDefaultNavigationTimeout(0);` after `const page = await this.browser.newPage();`
+        // thi action will turn off timeout completely
+        timeout: 120000 
     }
 })
 
 const keyword = process.argv[2]||'apple';
+const expectedResultCount = 5;
 
-/** Scrape for images link */
+/**
+ * Scrape for images link
+ * @param {string} keyword image keyword to search for
+ * @returns void
+ */
 async function ScrapeImg(keyword) {
     try {
-        const result = await google.scrape(keyword, 5);
+        const result = await google.scrape(keyword, expectedResultCount);
         let data = JSON.stringify(result, null, 4);
         fs.writeFileSync(`imagesLink/${trimText(keyword)}.json`, data);
         result.map((img, idx) => {
@@ -37,17 +47,18 @@ async function ScrapeImg(keyword) {
  * @param {string} url image url
  * @param {string} filename name to save the image with
  */
-function saveImage(url, fileName) {
-    fetch(url)
-        .then(res => {
-            const filePath = `./images/${trimText(fileName)}.jpg`;
-            const dest = fs.createWriteStream(filePath);
-            res.body.pipe(dest)
-        })
-        .catch((err) => {
-            console.log(`image ${filename} fetched from ${url} - failed!`)
-            console.log(err)
-        })
+async function saveImage(url, fileName) {
+    try {
+        console.log(`extracting and saving ${fileName}.jpg to file...`);
+        const res = await fetch(url);
+        const filePath = `./images/${trimText(fileName)}.jpg`;
+        const dest = fs.createWriteStream(filePath);
+        res.body.pipe(dest)
+        console.log(`image ${trimText(fileName)}.jpg saved!`)
+    } catch (error) {
+        console.log(`image ${fileName} fetched from ${url} - failed!`)
+        console.log(error.message);
+    }
 }
 
 function trimText(text) {
